a62f5fb199c64a244348af6c5eb09e85d2c4cfa8
dallas <dallas@DESKTOP-B21TAU4>
2020-11-05 11:49:44 -0700

    f

--- ddosprotection.py
+++ ddosprotection.py
@@ -14,36 +14,36 @@ active = []
 while True:
   data = requests.get(url, auth=(username, password)).content
   datadict = json.loads(data)
-  print(data)
-  datadict = {}
-  thedict = datadict['values']
-  # making list
-  for i in thedict:
-    #print(i)
-    for e in i:
-      #print(i[e])
-      #print(e)
-      ip = i[e]
-      m = re.search(r"(\d+\.\d+\.\d+\.\d+)\/\d+", ip)
-      if m:
-        ip = m.group(1)
-      key = e
-      if e == "ip":
-        ips.append(ip)
+  if len(data) > 0:
+    datadict = {}
+    thedict = datadict['values']
+    # making list
+    for i in thedict:
+      #print(i)
+      for e in i:
+        #print(i[e])
+        #print(e)
+        ip = i[e]
+        m = re.search(r"(\d+\.\d+\.\d+\.\d+)\/\d+", ip)
+        if m:
+          ip = m.group(1)
+        key = e
+        if e == "ip":
+          ips.append(ip)
  
-  # looking for new ddos
-  for i in ips:
-    #print("IP: "+i)
-    if i not in active:
-      print("Not in active")
-      active.append(i)
+    # looking for new ddos
+    for i in ips:
+      #print("IP: "+i)
+      if i not in active:
+        print("Not in active")
+        active.append(i)
 
 
-  # lets check to see if a ddos is gone
-  for i in active:
-    #print("Active IP: "+i)
-    if i not in ips:
-      print("Not longer in IPS")
-      active.remove(i)
+    # lets check to see if a ddos is gone
+    for i in active:
+      #print("Active IP: "+i)
+      if i not in ips:
+        print("Not longer in IPS")
+        active.remove(i)
 
   time.sleep(1)
