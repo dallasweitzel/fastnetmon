822a2930f6c0d8b61e31bd1bb96ef178ff345bb6
dallas <dallas@DESKTOP-B21TAU4>
2020-11-05 11:42:46 -0700

    f

--- ddosprotection.py
+++ ddosprotection.py
@@ -15,6 +15,7 @@ while True:
   data = requests.get(url, auth=(username, password)).content
   datadict = json.loads(data)
   thedict = datadict['values']
+  # making list
   for i in thedict:
     #print(i)
     for e in i:
@@ -28,10 +29,18 @@ while True:
       if e == "ip":
         ips.append(ip)
  
-
+  # looking for new ddos
   for i in ips:
     print("IP: "+i)
     if i not in active:
       print("Not in active")
       active.append(i)
+
+
+  # lets check to see if a ddos is gone
+  for i in active:
+    print("Active IP: "+i)
+    if i not in ips:
+      print("Not longer in IPS")
+
   time.sleep(1)
