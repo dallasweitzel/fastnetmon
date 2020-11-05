52e351b176d2efce5d206c5923766b789ca6590f
dallas <dallas@DESKTOP-B21TAU4>
2020-11-05 10:54:00 -0700

    f

--- ddosprotection.py
+++ ddosprotection.py
@@ -7,7 +7,7 @@ data = ""
 datadict = {}
 data = requests.get(url, auth=(username, password)).content
 output_json = json.loads(data)
-for i in output_json:
+for i in output_json[0]:
   print(i)
-  for k in output_json[i]:
-          print(k, output_json[1])
+  #for k in output_json[i]:
+  #        print(k, output_json[1])
