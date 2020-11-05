3a21c2d6afbf5f6b5b6ff52219c14c2fda7acf1b
dallas <dallas@DESKTOP-B21TAU4>
2020-11-05 11:02:37 -0700

    f

--- ddosprotection.py
+++ ddosprotection.py
@@ -6,8 +6,9 @@ password = '3110'
 data = ""
 datadict = {}
 data = requests.get(url, auth=(username, password)).content
-output_json = json.loads(data)
-for i in output_json['values']:
-  print(i[1])
-  #for k in output_json[i]:
-  #        print(k)
+blackholed_hosts = json.loads(data)
+
+blackholed_ip_addresses = []
+for blackholed_entity in blackholed_hosts.json()['values']:
+    blackholed_ip = blackholed_entity['ip'].replace('/32', '', -1)
+    blackholed_ip_addresses.append(blackholed_ip)
