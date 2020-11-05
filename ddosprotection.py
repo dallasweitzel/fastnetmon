13fed5a765656d91fe3548f71949c28a2cd9e119
dallas <dallas@DESKTOP-B21TAU4>
2020-11-05 11:46:50 -0700

    f

--- ddosprotection.py
+++ ddosprotection.py
@@ -6,7 +6,7 @@ url = 'http://157.245.130.37:10007/blackhole'
 username = 'admin'
 password = '3110'
 data = ""
-datadict = {}
+
 thedict = {}
 ips = []
 active = []
@@ -14,6 +14,7 @@ active = []
 while True:
   data = requests.get(url, auth=(username, password)).content
   datadict = json.loads(data)
+  datadict = {}
   thedict = datadict['values']
   # making list
   for i in thedict:
