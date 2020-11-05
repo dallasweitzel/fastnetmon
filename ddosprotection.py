85909a433ddbb4d9244044941ade595f6965ce9a
dallas <dallas@DESKTOP-B21TAU4>
2020-11-05 11:22:23 -0700

    f

--- ddosprotection.py
+++ ddosprotection.py
@@ -10,5 +10,7 @@ data = requests.get(url, auth=(username, password)).content
 datadict = json.loads(data)
 thedict = datadict['values']
 for i in thedict:
+  print(i)
   for e in i:
-    print(i[e][1])
+    print(i[e])
+ 
