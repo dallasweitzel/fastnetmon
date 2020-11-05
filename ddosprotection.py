dbb8d996a80a095e1466879151c2682513eb270c
dallas <dallas@DESKTOP-B21TAU4>
2020-11-05 10:46:52 -0700

    f

--- ddosprotection.py
+++ ddosprotection.py
@@ -6,14 +6,13 @@ password = '3110'
 data = ""
 datadict = {}
 data = requests.get(url, auth=(username, password)).content
-json_raw= data.readlines()
-datadict = json.loads(json_raw[0])
-print(str(data))
+json_object = json.load(data)
+print(str(json_object[0]['values']))
 
-for i in datadict:
-  line = datadict[i]
-  print("Each: "+str(line))
-  for k in line:
-    print(str(k))
-    #if k == "name":
-    #  gname = datadict[i][k]
+#for i in datadict:
+#  line = datadict[i]
+#  print("Each: "+str(line))
+#  for k in line:
+#    print(str(k))
+#    #if k == "name":
+#    #  gname = datadict[i][k]
